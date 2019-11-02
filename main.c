3ef08938b6e02b851bb1af4a449e7a4275a78e1d
Carlos Sosa <SosaSoft@users.noreply.github.com>
2019-11-02 10:13:20 -0600

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  */
 int main()
 {
-	char *str = "Hola Mundo, Somos los Debian, Ary Hernandez, Marc Guzman, Carlos Sosa, Jose Araujo, Andres Alvarez", *ch;
+	char *str = "Hola mundo, mi nombre es Teed, soy tu sistema operativo", *ch;
 	unsigned short *vidmem = (unsigned short*) 0xb8000;
 	unsigned i;
 	for (ch = str, i = 0; *ch; ch++, i++)
